d40ada19c02b1872652d0783d86a17be70a37f8e
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-10-13 15:02:24 +0300

    feat: add article repository with basic CRUD operations

---
+++ src/repositories/article.repository.js
@@ -0,0 +1,40 @@
+import ArticleModel from "../models/Article.model.js";
+
+class ArticleRepository {
+  async getArticleById(articleId) {
+    const article = await ArticleModel.findById(articleId)
+      .populate("author", "firstName lastName profilePicture zebedeeWalletId")
+      .populate("topic")
+      .populate("tags")
+      .lean();
+    return article;
+  }
+
+  async getArticles(query = {}, skip = 0, limit = 10, sort = { createdAt: -1 }) {
+    const articles = await ArticleModel.find(query)
+      .select("-content")
+      .skip(skip)
+      .limit(limit)
+      .sort(sort)
+      .lean();
+    return articles;
+  }
+
+  async createArticle(data) {
+    const result = await ArticleModel.create(data);
+    return result;
+  }
+
+  async updateArticleById(articleId, data) {
+    const result = await ArticleModel.findByIdAndUpdate(articleId, data, { new: true }).lean();
+    return result;
+  }
+
+  async deleteArticleById(articleId) {
+    const result = await ArticleModel.findByIdAndDelete(articleId);
+    return result;
+  }
+}
+
+const articleRepository = new ArticleRepository();
+export default articleRepository;
