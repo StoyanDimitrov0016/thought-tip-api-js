3bf6dc4338e1744a928fa680c9fe9d86d3d91269
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-10-14 11:22:00 +0300

    feat: add JWT generator function

---
+++ src/utils/createJWT.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import envConfig from "../config/envConfig.js";
+
+const JWT_SECRET = envConfig.jwtSecret;
+const JWT_EXPIRATION_TIME_IN_STRING = "7d";
+
+const generateToken = (user) => {
+  const payload = {
+    id: user._id,
+    email: user.email,
+    role: user.role,
+    username: user.username,
+  };
+
+  if (user.profilePicture?.trim()) {
+    payload.profilePicture = user.profilePicture;
+  }
+
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRATION_TIME_IN_STRING });
+  return token;
+};
+
+export default generateToken;
