750d2713356c9721c5c8efc6e044e7a157c2ff21
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-10-15 12:54:22 +0300

    feat: add zod validation schema for the register request body object

---
+++ src/schemas/zod/auth-requests/registerValidationSchema.js
@@ -0,0 +1,36 @@
+import z from "zod";
+
+const registerValidationSchema = z.object({
+  email: z.string().email(),
+  username: z
+    .string()
+    .min(3)
+    .max(20)
+    .regex(/^[a-zA-Z0-9_.]+$/, "Username can only contain letters, numbers, underscores, or dots"),
+  password: z
+    .string()
+    .min(8)
+    .max(64)
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/,
+      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    ),
+  firstName: z
+    .string()
+    .min(2)
+    .max(30)
+    .regex(
+      /^[A-Z][a-z'-]+$/,
+      "First name must start with a capital letter and can only contain letters, hyphens, or apostrophes"
+    ),
+  lastName: z
+    .string()
+    .min(2)
+    .max(30)
+    .regex(
+      /^[A-Z][a-z'-]+$/,
+      "Last name must start with a capital letter and can only contain letters, hyphens, or apostrophes"
+    ),
+});
+
+export default registerValidationSchema;
