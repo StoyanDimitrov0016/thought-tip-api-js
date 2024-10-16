b4757adc41bd9a06c2302253fbfb9f0829aaafa5
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-10-16 23:52:24 +0300

    feat: add better error handling and messages

--- src/middleware/isAuthenticated.js
+++ src/middleware/isAuthenticated.js
@@ -4,20 +4,21 @@ import envConfig from "../config/envConfig.js";
 const JWT_SECRET = envConfig.jwtSecret;
 
 const isAuthenticated = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).json({ message: "Authentication token is missing" });
-  }
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Authentication token is missing" });
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
-
     req.user = { id: decoded.id };
 
     next();
   } catch (error) {
-    return res.status(403).json({ message: "Invalid or expired token" });
+    if (error.name === "TokenExpiredError") {
+      return res.status(403).json({ message: "Token has expired" });
+    } else if (error.name === "JsonWebTokenError") {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+    return res.status(500).json({ message: "Authentication failed" });
   }
 };
 
