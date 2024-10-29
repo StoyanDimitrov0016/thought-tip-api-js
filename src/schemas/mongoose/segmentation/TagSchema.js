0a658fa56b17ff7d696b2fbd3af7144df5bdf485
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-10-29 14:05:30 +0200

    refactor: enhance Tag schema to support name, slug, description, and status fields with validation, and add topic reference

--- src/schemas/mongoose/segmentation/TagSchema.js
+++ src/schemas/mongoose/segmentation/TagSchema.js
@@ -1,17 +1,52 @@
 import { Schema, Types } from "mongoose";
 
-const TagSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Tag name is required"],
-    unique: true,
-    trim: true,
+const TagSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Tag name is required"],
+      unique: true,
+      trim: true,
+      minlength: [2, "Tag name must be at least 2 characters"],
+      maxlength: [30, "Tag name cannot exceed 30 characters"],
+    },
+    slug: {
+      type: String,
+      required: [true, "Slug is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      minlength: [2, "Slug must be at least 2 characters"],
+      maxlength: [30, "Slug cannot exceed 30 characters"],
+    },
+    description: {
+      type: String,
+      trim: true,
+      minlength: [10, "Description must be at least 10 characters"],
+      maxlength: [150, "Description cannot exceed 150 characters"],
+    },
+    topicId: {
+      type: Types.ObjectId,
+      ref: "Topic",
+      required: [true, "Topic ID is required"],
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["active", "inactive", "archived", "in development"],
+        message:
+          "Invalid status! Supported statuses are: 'active', 'inactive', 'archived', 'in development'.",
+      },
+      default: "in development",
+    },
   },
-  topicId: {
-    type: Types.ObjectId,
-    ref: "Topic",
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+TagSchema.index({ slug: 1 });
+TagSchema.index({ name: 1 });
+TagSchema.index({ topicId: 1 });
 
 export default TagSchema;
