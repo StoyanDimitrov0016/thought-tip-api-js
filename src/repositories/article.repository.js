3e7a2d0ce7f343cbc170685adae76d5c197c4b3a
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-11-02 16:23:58 +0200

    refactor: method naming change, id validation implementation, better population and fields extraction usage

--- src/repositories/article.repository.js
+++ src/repositories/article.repository.js
@@ -1,62 +1,81 @@
 import ArticleModel from "../models/Article.model.js";
+import { validateMongooseObjectId } from "../utils/mongooseIdsValidators.js";
 
 class ArticleRepository {
-  async getArticleById(articleId) {
-    const article = await ArticleModel.findById(articleId)
+  async findOneById(id) {
+    const idValidationResult = validateMongooseObjectId(id, false);
+    if (!idValidationResult) {
+      return null;
+    }
+
+    const article = await ArticleModel.findById(id)
       .populate("author", "firstName lastName profilePicture zebedeeWalletId")
-      .populate("topic")
+      .populate("category")
+      .populate("topics")
       .populate("tags")
       .lean();
+
     return article;
   }
 
-  async getArticles(query = {}, skip = 0, limit = 10, sort = { createdAt: -1 }) {
+  async findOneByQueryWithoutId(queryWithoutId) {
+    const article = await ArticleModel.findOne(queryWithoutId).lean();
+    return article;
+  }
+
+  async findManyByQuery(query = {}, skip = 0, limit = 10, sort = { createdAt: -1 }) {
     const articles = await ArticleModel.find(query)
-      .select("-content") // Exclude content field if you want a summary
-      .populate("author", "firstName lastName profilePicture") // Populate author details
-      .populate("topic", "name") // Populate topic name
-      .populate({
-        path: "tags", // Populate tags array
-        select: "name topicId", // Specify the fields to be populated for tags
-        populate: { path: "topicId", select: "name" }, // Nested populate for topic inside each tag (optional)
-      })
+      .select("-content")
+      .populate("author", "firstName lastName profilePicture zebedeeWalletId")
+      .populate("category")
+      .populate("topics")
+      .populate("tags")
       .skip(skip)
       .limit(limit)
       .sort(sort)
       .lean();
+
     return articles;
   }
 
-  async createArticle(data) {
-    try {
-      console.log("from repo - data", data);
+  async createOne(data) {
+    const createdArticle = await ArticleModel.create(data);
+    return createdArticle;
+  }
 
-      const result = await ArticleModel.create(data);
-      console.log("from repo - result", result);
-      return result;
-    } catch (error) {
-      console.log(error);
+  async updateOneById(id, data) {
+    const idValidationResult = validateMongooseObjectId(id, false);
+    if (!idValidationResult) {
+      return null;
     }
+
+    const updatedArticle = await ArticleModel.findByIdAndUpdate(id, data, { new: true }).lean();
+    return updatedArticle;
   }
 
-  async updateArticleById(articleId, data) {
-    const result = await ArticleModel.findByIdAndUpdate(articleId, data, { new: true }).lean();
-    return result;
+  async deleteOneById(id) {
+    const idValidationResult = validateMongooseObjectId(id, false);
+    if (!idValidationResult) {
+      return null;
+    }
+
+    const deletingResult = await ArticleModel.findByIdAndDelete(id).lean();
+    return deletingResult;
   }
 
-  async deleteArticleById(articleId) {
-    const result = await ArticleModel.findByIdAndDelete(articleId);
-    return result;
-  }
-
-  async getArticlesCount(query) {
+  async findCountByQuery(query) {
     const count = await ArticleModel.countDocuments(query);
     return count;
   }
 
-  async isUserArticleAuthor(articleId, userId) {
-    const matchedArticle = await ArticleModel.exists({ _id: articleId, author: userId });
-    return !!matchedArticle;
+  async doesUserTheArticleAuthor(userId) {
+    const idValidationResult = validateMongooseObjectId(userId, false);
+    if (!idValidationResult) {
+      return null;
+    }
+
+    const match = await ArticleModel.exists({ author: userId }).lean();
+    return match;
   }
 }
 
