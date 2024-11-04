19bf57eb70fd877b7cf38c7e86a265d2574f3319
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-11-04 23:20:52 +0200

    feat(middleware): extend appendUserIfExists to include verification level and external user ID in request object

--- src/middleware/appendUserIfExists.js
+++ src/middleware/appendUserIfExists.js
@@ -4,15 +4,17 @@ import envConfig from "../config/envConfig.js";
 const JWT_SECRET = envConfig.jwtSecret;
 
 const appendUserIfExists = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
 
   if (token) {
     try {
       const decoded = jwt.verify(token, JWT_SECRET);
-
-      req.user = { id: decoded.id };
-
-      req.user.role = decoded.role;
+      req.user = {
+        id: decoded.id,
+        role: decoded.role,
+        verificationLevel: decoded.verificationLevel,
+        externalUserId: decoded.externalUserId,
+      };
     } catch (error) {
       console.log("Invalid or expired token");
     }
