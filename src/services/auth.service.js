f0d036e1dc155d0e695fd787f8ea29f76edf0f49
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-11-05 18:22:29 +0200

    refactor(auth service): convert to class-based structure and integrate updated user repository methods

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,42 +1,72 @@
 import userRepository from "../repositories/user.repository.js";
 import generateToken from "../utils/generateJWT.js";
 import { comparePasswords, hashPassword } from "../utils/passwordUtils.js";
+import { INITIAL_USER_ARTICLE_COUNT, INITIAL_USER_ARTICLE_LIMIT } from "../constants/user.js";
 
-const userLogin = async (credentials) => {
-  const { email, username, password } = credentials;
+class AuthService {
+  async login(credentials) {
+    try {
+      const { email, username, password } = credentials;
 
-  const existingUser = await userRepository.getUserByEmailOrUsername(email, username);
-  if (!existingUser) throw new Error("Invalid credentials");
+      const existingUser = await userRepository.findOneByQueryWithoutIds({
+        $or: [{ email }, { username }],
+      });
+      if (!existingUser) {
+        throw new Error("Invalid credentials");
+      }
 
-  const isPasswordMatch = await comparePasswords(password, existingUser.password);
-  if (!isPasswordMatch) throw new Error("Invalid credentials");
+      const doesPasswordsMatch = await comparePasswords(password, existingUser.encryptedPassword);
+      if (!doesPasswordsMatch) {
+        throw new Error("Invalid credentials");
+      }
 
-  const { password: _, ...user } = existingUser;
-  const token = generateToken(user);
+      const { encryptedPassword: _, bio, ...user } = existingUser;
+      const token = generateToken(user);
+      return { user, token };
+    } catch (error) {
+      throw error;
+    }
+  }
 
-  return { user, token };
-};
+  async register(registrationData) {
+    try {
+      const { email, username, password, firstName, lastName } = registrationData;
 
-const userRegister = async (registrationData) => {
-  const { email, username, password, firstName, lastName } = registrationData;
+      const existingUserPartial = await userRepository.checkExistenceOfOne({
+        $or: [{ email }, { username }],
+      });
+      if (existingUserPartial) {
+        throw new Error("Email or username already taken");
+      }
 
-  const existingUser = await userRepository.getUserByEmailOrUsername(email, username);
-  if (existingUser) throw new Error("Email or username already taken");
+      const encryptedPassword = await hashPassword(password);
 
-  const hashedPassword = await hashPassword(password);
+      const createdUser = await userRepository.createOne({
+        email,
+        username,
+        encryptedPassword,
+        firstName,
+        lastName,
+        role: "regular",
+        verificationLevel: "none",
+        articleLimit: INITIAL_USER_ARTICLE_LIMIT,
+        userArticleCount: INITIAL_USER_ARTICLE_COUNT,
+      });
+      const { encryptedPassword: _, bio, ...user } = createdUser;
 
-  const createdUser = await userRepository.createUser({
-    email,
-    username,
-    password: hashedPassword,
-    firstName,
-    lastName,
-  });
-  const { password: _, ...user } = createdUser;
+      const token = generateToken(user);
+      return { user, token };
+    } catch (error) {
+      throw error;
+    }
+  }
 
-  const token = generateToken(user);
-  return { user, token };
-};
+  //TODO: build these methods:
+  /*
+    async changePassword() {}
+    async forgotPassword() {}
+  */
+}
 
-const authService = { userLogin, userRegister };
+const authService = new AuthService();
 export default authService;
