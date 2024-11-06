f6cee2cca3f600b11fc673c67c7730179eb01d43
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-11-06 17:51:20 +0200

    feat(auth): create jwtManager for centralized JWT signing, verification, and decoding with integrated jwtErrorHandler

---
+++ src/utils/jwtManager.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import envConfig from "../config/envConfig.js";
+import jwtErrorHandler from "./handlers/jwtErrorHandler.js";
+
+const JWT_SECRET = envConfig.jwtSecret;
+const EXPIRATION_DURATION = "7d";
+
+class JWTManager {
+  signToken(payload, options = {}) {
+    try {
+      return jwt.sign(payload, JWT_SECRET, { expiresIn: EXPIRATION_DURATION, ...options });
+    } catch (error) {
+      jwtErrorHandler(error);
+    }
+  }
+
+  verifyAndDecodeToken(token, options = {}) {
+    try {
+      return jwt.verify(token, JWT_SECRET, options);
+    } catch (error) {
+      jwtErrorHandler(error);
+    }
+  }
+
+  decodeToken(token) {
+    return jwt.decode(token);
+  }
+}
+
+const jwtManager = new JWTManager();
+export default jwtManager;
