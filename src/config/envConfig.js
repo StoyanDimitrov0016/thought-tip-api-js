e355fd0e84faa17d1b935f58dc05359428447f12
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-11-06 17:53:04 +0200

    feat(env): add NODE_ENV variable to envConfig for environment-specific configurations used across the application

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -3,8 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const envConfig = {
-  port: process.env.PORT || 5000,
+  port: process.env.PORT,
   mongoUri: process.env.MONGO_URI,
   jwtSecret: process.env.JWT_SECRET,
+  nodeEnv: process.env.NODE_ENV,
 };
+
 export default envConfig;
