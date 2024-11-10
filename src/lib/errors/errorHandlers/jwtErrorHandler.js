46312d27387c2a69073f351dce43133d4e54a42e
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-11-10 22:00:08 +0200

    chore: fix imports and add log when exception error occurs

--- src/lib/errors/errorHandlers/jwtErrorHandler.js
+++ src/lib/errors/errorHandlers/jwtErrorHandler.js
@@ -1,25 +1,26 @@
-import { UnauthorizedError } from "../errors/customErrors.js";
+import { UnauthorizedError } from "../customErrors/ErrorSubclasses.js";
 
 const jwtErrorHandler = (error) => {
   if (error.name === "TokenExpiredError") {
-    throw new UnauthorizedError("Token has expired.", "/", [
+    throw new UnauthorizedError("Token has expired.", [
       { field: "token", message: "Token has expired." },
     ]);
   }
 
   if (error.name === "JsonWebTokenError") {
-    throw new UnauthorizedError("Invalid token.", "/", [
+    throw new UnauthorizedError("Invalid token.", [
       { field: "token", message: "Invalid authentication token." },
     ]);
   }
 
   if (error.name === "NotBeforeError") {
-    throw new UnauthorizedError("Token is not yet valid.", "/", [
+    throw new UnauthorizedError("Token is not yet valid.", [
       { field: "token", message: "Token is not yet active." },
     ]);
   }
 
-  throw new UnauthorizedError("JWT processing error.", "/", [
+  console.error("---Unexpected JWT error:", error);
+  throw new UnauthorizedError("JWT processing error.", [
     { field: "token", message: "Unexpected JWT error occurred." },
   ]);
 };
