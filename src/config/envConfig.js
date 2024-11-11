a2602ff500924296bf639f13e65f44527ede72ce
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-11-11 23:27:52 +0200

    feat: add base url property to the environment config object

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const envConfig = {
   port: process.env.PORT,
+  baseURL: process.env.BASE_URL,
   mongoUri: process.env.MONGO_URI,
   jwtSecret: process.env.JWT_SECRET,
   nodeEnv: process.env.NODE_ENV,
