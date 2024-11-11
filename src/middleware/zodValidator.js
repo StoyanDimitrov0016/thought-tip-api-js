446f99db369d07cfa8a95d64b07b1142d7a42f1d
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-11-11 23:33:18 +0200

    feat: add custom error and zod error formatting

--- src/middleware/zodValidator.js
+++ src/middleware/zodValidator.js
@@ -1,12 +1,20 @@
+import { ZodError } from "zod";
+import { ValidationError } from "../lib/errors/customErrors/ErrorSubclasses.js";
+
+//TODO: add different req properties apart from the body
 const zodValidator = (schema) => (req, res, next) => {
   try {
     schema.parse(req.body);
     next();
   } catch (error) {
-    return res.status(400).json({
-      message: "Validation error",
-      errors: error.errors,
-    });
+    if (error instanceof ZodError) {
+      const formattedErrors = error.errors.map((issue) => ({
+        field: issue.path.join("."),
+        message: issue.message,
+      }));
+      return next(new ValidationError("Validation failed", formattedErrors));
+    }
+    next(error);
   }
 };
 
