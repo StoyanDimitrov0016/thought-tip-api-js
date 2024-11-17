97c2ebbc7de8426586d4f346e4616c92b660af9b
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-11-17 17:18:12 +0200

    refactor(router): update CategoryRouter to use `getAll` instead of `getMany` in CategoryController

--- src/routes/segmentation/category.route.js
+++ src/routes/segmentation/category.route.js
@@ -6,7 +6,7 @@ import checkSpecialAccess from "../../middleware/checkSpecialAccess.js";
 const categoryRouter = Router();
 
 categoryRouter.get("/:slug", categoryController.getOneBySlug);
-categoryRouter.get("/", categoryController.getMany);
+categoryRouter.get("/", categoryController.getAll);
 categoryRouter.post("/", isAuthenticated, checkSpecialAccess, categoryController.createOne);
 categoryRouter.patch("/:id", isAuthenticated, checkSpecialAccess, categoryController.updateOneById);
 
