132ce8cfabbd2f359be530706d023600837b9a5e
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-11-18 22:55:38 +0200

    feat: implement global error handler invoking with custom forbidden error instead of simple response with only message property

--- src/middleware/checkSpecialAccess.js
+++ src/middleware/checkSpecialAccess.js
@@ -1,10 +1,17 @@
-const specialAccessUserIds = ["670e440ca088c220040c8e51"];
+import { ForbiddenError } from "../lib/errors/customErrors/ErrorSubclasses.js";
+
+const specialAccessUserIds = ["673367b12a9cc2a7567387b4"];
 
 const checkSpecialAccess = (req, res, next) => {
   const userId = req.user.id;
 
   const isAllowed = specialAccessUserIds.includes(userId);
-  if (!isAllowed) return res.status(403).json({ message: "Special access required" });
+  if (!isAllowed) {
+    const error = new ForbiddenError("Special access required to proceed.", [
+      { field: "unauthorized", message: "Special access only" },
+    ]);
+    return next(error);
+  }
 
   next();
 };
