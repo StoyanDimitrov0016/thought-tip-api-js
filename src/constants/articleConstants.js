ca25cd23b69e3d3a69b5d368a58a283cccfea34f
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-11-22 23:26:01 +0200

    feat: add article constants for validation and default values
    - Defined `ARTICLE_SLUG_REGEX` for validating article slugs.
    - Set `ARTICLE_MIN_READING_TIME` and `ARTICLE_MAX_READING_TIME` to constrain reading time range.
    - Introduced `ARTICLE_MIN_CHARGE` and `ARTICLE_MAX_CHARGE` for handling article paywall charges.
    - Added `ARTICLE_DISCUSSION_DEFAULT_VALUE` for setting the default discussion state of articles.

---
+++ src/constants/articleConstants.js
@@ -0,0 +1,9 @@
+export const ARTICLE_SLUG_REGEX = /^[a-z0-9]+(?:-[a-z0-9]+)*$/;
+
+export const ARTICLE_MIN_READING_TIME = 1;
+export const ARTICLE_MAX_READING_TIME = 100;
+
+export const ARTICLE_MIN_CHARGE = 0;
+export const ARTICLE_MAX_CHARGE = 1000;
+
+export const ARTICLE_DISCUSSION_DEFAULT_VALUE = true;
