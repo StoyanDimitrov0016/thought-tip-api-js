0b42eb1adb5401f847672a5a7c7e3a8f3d71a1cd
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-11-22 16:37:00 +0200

    feat: update special access middleware to support 'client' naming convention

--- src/middleware/checkSpecialAccess.js
+++ src/middleware/checkSpecialAccess.js
@@ -3,9 +3,9 @@ import { ForbiddenError } from "../lib/errors/customErrors/ErrorSubclasses.js";
 const specialAccessUserIds = ["673367b12a9cc2a7567387b4"];
 
 const checkSpecialAccess = (req, res, next) => {
-  const userId = req.user.id;
+  const clientId = req.client.id;
 
-  const isAllowed = specialAccessUserIds.includes(userId);
+  const isAllowed = specialAccessUserIds.includes(clientId);
   if (!isAllowed) {
     const error = new ForbiddenError("Special access required to proceed.", [
       { field: "unauthorized", message: "Special access only" },
