231bbd08d537857d19194a70ca8d4833d74972db
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-12-09 19:57:21 +0200

    feat: enhance tag routes with validation and controller integration
    - Integrated `tagController` methods for CRUD operations.
    - Added `idValidator` middleware to validate `tagId` route parameters.
    - Implemented `inputValidator` middleware for request body validation using `createSchema` and `updateSchema`.
    - Improved route definitions for `GET`, `POST`, `PUT`, and `DELETE` methods.
    - Enhanced maintainability and ensured data integrity with centralized validation logic.

--- src/routes/segmentation/tag.routes.js
+++ src/routes/segmentation/tag.routes.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
+import inputValidator from "../../middleware/validators/inputValidator.js";
+import { idValidator } from "../../middleware/validators/idValidator.js";
+import { tagController as controller } from "../../controllers/segmentation.controller.js";
+import { createSchema, updateSchema } from "../../lib/validationSchemas/segmentation/client.js";
 
+const id = "tagId";
 const tagRoutes = Router();
 
-tagRoutes.get("/");
-tagRoutes.get("/:id");
+tagRoutes.get("/", controller.getAll);
+tagRoutes.get(`/${id}`, idValidator(id), controller.getOne);
 
-tagRoutes.post("/");
-tagRoutes.put("/:id");
-tagRoutes.delete("/:id");
+tagRoutes.post("/", inputValidator(createSchema), controller.create);
+tagRoutes.put(`/${id}`, idValidator(id), inputValidator(updateSchema), controller.update);
+tagRoutes.delete(`/${id}`, idValidator(id), controller.remove);
 
 export default tagRoutes;
