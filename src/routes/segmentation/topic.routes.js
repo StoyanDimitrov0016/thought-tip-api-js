0d6add4f22d049353a71438cc7489ac8ee4e9409
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-12-09 19:57:06 +0200

    feat: enhance topic routes with validation and controller integration
    - Integrated `topicController` methods for CRUD operations.
    - Added `idValidator` middleware to validate `topicId` route parameters.
    - Implemented `inputValidator` middleware for request body validation using `createSchema` and `updateSchema`.
    - Improved route definitions for `GET`, `POST`, `PUT`, and `DELETE` methods.
    - Enhanced maintainability and ensured data integrity with centralized validation logic.

--- src/routes/segmentation/topic.routes.js
+++ src/routes/segmentation/topic.routes.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
+import inputValidator from "../../middleware/validators/inputValidator.js";
+import { idValidator } from "../../middleware/validators/idValidator.js";
+import { topicController as controller } from "../../controllers/segmentation.controller.js";
+import { createSchema, updateSchema } from "../../lib/validationSchemas/segmentation/client.js";
 
+const id = "categoryId";
 const topicRoutes = Router();
 
-topicRoutes.get("/");
-topicRoutes.get("/:id");
+topicRoutes.get("/", controller.getAll);
+topicRoutes.get(`/${id}`, idValidator(id), controller.getOne);
 
-topicRoutes.post("/");
-topicRoutes.put("/:id");
-topicRoutes.delete("/:id");
+topicRoutes.post("/", inputValidator(createSchema), controller.create);
+topicRoutes.put(`/${id}`, idValidator(id), inputValidator(updateSchema), controller.update);
+topicRoutes.delete(`/${id}`, idValidator(id), controller.remove);
 
 export default topicRoutes;
