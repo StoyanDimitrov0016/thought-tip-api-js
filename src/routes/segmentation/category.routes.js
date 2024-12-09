e7598b45495e90f858cbab72fc423c087321a14e
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-12-09 19:54:50 +0200

    feat: enhance category routes with validation and controller integration
    - Integrated `categoryController` methods for handling CRUD operations.
    - Added `idValidator` middleware to validate `categoryId` route parameters.
    - Implemented `inputValidator` middleware for request body validation using `createSchema` and `updateSchema`.
    - Improved route definitions for `GET`, `POST`, `PUT`, and `DELETE` methods.
    - Enhanced maintainability and ensured data integrity with centralized validation logic.

--- src/routes/segmentation/category.routes.js
+++ src/routes/segmentation/category.routes.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
+import inputValidator from "../../middleware/validators/inputValidator.js";
+import { idValidator } from "../../middleware/validators/idValidator.js";
+import { categoryController as controller } from "../../controllers/segmentation.controller.js";
+import { createSchema, updateSchema } from "../../lib/validationSchemas/segmentation/client.js";
 
+const id = "categoryId";
 const categoryRoutes = Router();
 
-categoryRoutes.get("/");
-categoryRoutes.get("/:id");
+categoryRoutes.get("/", controller.getAll);
+categoryRoutes.get(`/${id}`, idValidator(id), controller.getOne);
 
-categoryRoutes.post("/");
-categoryRoutes.put("/:id");
-categoryRoutes.delete("/:id");
+categoryRoutes.post("/", inputValidator(createSchema), controller.create);
+categoryRoutes.put(`/${id}`, idValidator(id), inputValidator(updateSchema), controller.update);
+categoryRoutes.delete(`/${id}`, idValidator(id), controller.remove);
 
 export default categoryRoutes;
