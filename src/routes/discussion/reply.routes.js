81116a724f904f7bd67d9b9aa90dad638ee36970
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-12-10 16:15:33 +0200

    feat: add reply routes with validation and integration with discussion controller
    - Added `reply.routes.js` with CRUD routes.
    - Integrated `inputValidator` and `idValidator` for schema and parameter validation.
    - Used `replyController` for handling reply-specific requests."

--- src/routes/discussion/reply.routes.js
+++ src/routes/discussion/reply.routes.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
+import { replyController as controller } from "../../controllers/discussion.controller.js";
+import inputValidator from "../../middleware/validators/inputValidator.js";
+import { idValidator } from "../../middleware/validators/idValidator.js";
+import { upcomingSchemas } from "../../lib/validationSchemas/discussion.js";
 
-const replyRoutes = Router();
+const { create: createSchema, update: updateSchema } = upcomingSchemas;
+const id = "id";
+const router = Router();
 
-replyRoutes.get("/");
-replyRoutes.get("/:id");
+router.get("/", controller.getAll);
+router.get(`/:${id}`, idValidator(id), controller.getOne);
 
-replyRoutes.post("/");
-replyRoutes.put("/:id");
-replyRoutes.delete("/:id");
+router.post("/", inputValidator(createSchema), controller.create);
+router.put(`/:${id}`, inputValidator(updateSchema), idValidator(id), controller.update);
+router.delete(`/:${id}`, idValidator(id), controller.remove);
 
-export default replyRoutes;
+export { router };
