82924843f280c76b02dca1c86152710b04927040
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-12-10 14:26:11 +0200

    refactor: replace req.client with req.user to prevent conflicts and ensure user type integrity for all requests
    - Middleware now sets default req.user values (id, role, username) even for public routes.
    - Maintains consistent user type and properties across all requests.

--- src/middleware/validators/tokenValidator.js
+++ src/middleware/validators/tokenValidator.js
@@ -2,12 +2,13 @@ import tokenManager from "../../lib/managers/tokenManager.js";
 
 export default function tokenValidator(req, res, next) {
   try {
-    const token = req.cookies?.token;
+    req.user = { id: null, role: null, username: null };
 
+    const token = req.cookies?.token;
     if (token) {
       try {
         const decoded = tokenManager.verify(token);
-        req.client = decoded;
+        req.user = decoded;
       } catch (error) {
         if (error.name === "TokenExpiredError") {
           res.clearCookie("token");
