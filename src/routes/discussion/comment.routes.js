664124eadf04a7d4f182cd8a6c223a9aa09b7515
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-12-10 16:15:55 +0200

    feat: add comment routes with validation and integration with discussion controller
    - Added `comment.routes.js` with CRUD routes.
    - Integrated `inputValidator` and `idValidator` for schema and parameter validation.
    - Used `commentController` for handling comment-specific requests.

--- src/routes/discussion/comment.routes.js
+++ src/routes/discussion/comment.routes.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
+import { commentController as controller } from "../../controllers/discussion.controller.js";
+import inputValidator from "../../middleware/validators/inputValidator.js";
+import { idValidator } from "../../middleware/validators/idValidator.js";
+import { upcomingSchemas } from "../../lib/validationSchemas/discussion.js";
 
-const commentRoutes = Router();
+const { create: createSchema, update: updateSchema } = upcomingSchemas;
+const id = "id";
+const router = Router();
 
-commentRoutes.get("/");
-commentRoutes.get("/:id");
+router.get("/", controller.getAll);
+router.get(`/:${id}`, idValidator(id), controller.getOne);
 
-commentRoutes.post("/");
-commentRoutes.put("/:id");
-commentRoutes.delete("/:id");
+router.post("/", inputValidator(createSchema), controller.create);
+router.put(`/:${id}`, inputValidator(updateSchema), idValidator(id), controller.update);
+router.delete(`/:${id}`, idValidator(id), controller.remove);
 
-export default commentRoutes;
+export { router };
