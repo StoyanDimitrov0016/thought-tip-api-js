0c104a29895b98d2ca5e05ed8700ff66025c016e
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-12-12 13:15:03 +0200

    remove outdated utility functions

--- src/utils/personalizeArticle.js
+++
@@ -1,7 +0,0 @@
-export const personalizeArticle = (article, userId) => {
-  const isAuthor = article.author._id.toString() === userId;
-  // TODO: check if the user has liked the article also after adding like pipeline
-
-  article["isAuthor"] = isAuthor;
-  return article;
-};
